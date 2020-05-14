c909c2c8066c844d30650850121b634ea35536c0
hanasuey <ouaoudahana@gmail.com>
2020-05-14 20:43:53 +0200

    v&d10

---
+++ 2-Variabelen-en-datatype/taak10/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $item = "<li>wacht</li>";
+    $item .= "<li>wat</li>";
+    $item .= "<li>patat</li>";
+    echo "<ul>$item</ul>";
+?>
+</body>
+
+</html>
