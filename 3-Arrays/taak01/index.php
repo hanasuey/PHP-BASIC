b9d5e7544cdc3e0ce569588505a33cd54b42f6da
hanasuey <ouaoudahana@gmail.com>
2020-05-14 20:58:27 +0200

    arrays1

---
+++ 3-Arrays/taak01/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $myMovies = array("Joker", "The Kingsman: Secret Service", "Inception");
+    echo "<ul><li>$myMovies[0]</li><li>$myMovies[1]</li><li>$myMovies[2]</li></ul>";
+?>
+</body>
+
+</html>
