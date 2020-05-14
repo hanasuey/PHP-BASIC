e24a303d0574c2fb3f54fd64d2f8dba006a095c2
hanasuey <ouaoudahana@gmail.com>
2020-05-14 20:35:20 +0200

    v&d9

---
+++ 2-Variabelen-en-datatype/taak09/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $leeftijd = '19';
+    $voornaam = 'Hana';
+    echo "Mijn naam is " . $voornaam . " en ik bent " . $leeftijd . " jaar oud.";
+?>
+</body>
+
+</html>
