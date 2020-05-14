3bb080dc863bcb466b7f16b4134e6d67b148203e
hanasuey <ouaoudahana@gmail.com>
2020-05-14 22:26:10 +0200

    conditional3

---
+++ 5-Conditional_Statements/taak03/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $variabel1 = "waarde1";
+    $variabel2 = "waarde2";
+    $variabel3 = "waarde3";
+    $variabel4 = "waarde4";
+    $variabel5 = "waarde5";
+    $variabel6 = "waarde6";
+    $variabel7 = "waarde7";
+    $variabel8 = "waarde8";
+    $variabel9 = "waarde9";
+    $variabel10 = "waarde10";
+        echo var_dump($variabel1 == $variabel2) . "</br>";
+        echo var_dump($variabel2 === $variabel3) . "</br>";
+        echo var_dump($variabel3 != $variabel4) . "</br>";
+        echo var_dump($variabel4 > $variabel5) . "</br>";
+        echo var_dump($variabel5 < $variabel6) . "</br>";
+        echo var_dump($variabel7 <> $variabel8) . "</br>";
+        echo var_dump($variabel8 >= $variabel9) . "</br>";
+        echo var_dump($variabel9 <= $variabel10) . "</br>";
+
+?>
+</body>
+
+</html>
