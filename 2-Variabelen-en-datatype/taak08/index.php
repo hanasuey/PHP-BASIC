58aac65cbecdfb7c2e4c37a0ab6c91825e878723
hanasuey <ouaoudahana@gmail.com>
2020-05-14 20:32:43 +0200

    v&d8

---
+++ 2-Variabelen-en-datatype/taak08/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $voornaam3 = 'Hana';
+    $voornaam2 = 'Danniele';
+    $voornaam3 = 'Janro';
+    echo "<li>$voornaam1</li><li>$voornaam2</li><li>$voornaam3</li>";
+?>
+</body>
+
+</html>
