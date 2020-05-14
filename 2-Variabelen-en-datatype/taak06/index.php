693ddc5eb67c751eb9c176e25691bccceeb6a9e0
hanasuey <ouaoudahana@gmail.com>
2020-05-14 20:27:57 +0200

    v&d6

---
+++ 2-Variabelen-en-datatype/taak06/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $voornaam = 'tom';
+    $achternaam = 5;
+    $bla = 5,5;
+    $test = false;
+    $iets = null;
+    echo gettype($voornaam, $achternaam, $bla, $test, $iets);
+?>
+</body>
+
+</html>
+
+gettype();
