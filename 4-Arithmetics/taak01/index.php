e4b7ffce73ce99aa9b5ac2bd516d05549c46758c
hanasuey <ouaoudahana@gmail.com>
2020-05-14 21:12:37 +0200

    arithmetics1

---
+++ 4-Arithmetics/taak01/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $getal1 = 5;
+    $getal2 = 4;
+    echo $getal1 + $getal2 . "</br>";
+    echo $getal1 - $getal2 . "</br>";
+    echo $getal1 * $getal2 . "</br>";
+    echo $getal1 / $getal2 . "</br>";
+?>
+</body>
+
+</html>
