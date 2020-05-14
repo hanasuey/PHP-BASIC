2f8d51c275c69b81ecbb4fcfea8214ca5dc37993
hanasuey <ouaoudahana@gmail.com>
2020-05-14 21:16:40 +0200

    arithmetics2

---
+++ 4-Arithmetics/taak02/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+    $getal1 = 9;
+    $getal1 += 8;
+    echo $getal1 . "</br>";
+
+    $getal1 = 9;
+    $getal1 -= 8;
+    echo $getal1 . "</br>";
+
+    $getal1 = 9;
+    $getal1 /= 8;
+    echo $getal1 . "</br>";
+
+    $getal1 = 9;
+    $getal1 *= 8;
+    echo $getal1 . "</br>";
+?>
+</body>
+
+</html>
