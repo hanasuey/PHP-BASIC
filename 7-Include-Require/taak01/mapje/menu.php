7503af09b9cfbba92cc975b93bd92bc5248b39e5
hanasuey <ouaoudahana@gmail.com>
2020-05-19 19:59:44 +0200

    includetaak1mapje

---
+++ 7-Include-Require/taak01/mapje/menu.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+
+<html>
+
+<body>
+<?php
+
+$item3 = 'item1';
+$item2 = 'item2';
+$item1 = 'item3';
+echo "</u><li>$item1</li><li>$item2</li><li>$item3</li></ul>";
+?>
+</body>
+
+</html>
