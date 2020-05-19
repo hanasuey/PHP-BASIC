ca87d1c5d12455c9ae324c38ceb2b6ce75ec4c9d
hanasuey <ouaoudahana@gmail.com>
2020-05-19 19:45:27 +0200

    conditional6

---
+++ 6-Functions/taak01/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+
+<html>
+<body>
+<?php
+
+
+
+function zoekLetterInAlfabet() {
+    $woordje = 'ja';
+    $zinnetje = 'nee ja nee. ja';
+    $helezin = $woordje . $zinnetje;
+    echo $helezin;
+}
+zoekLetterInAlfabet();
+
+?>
+</body>
+
+</html>
